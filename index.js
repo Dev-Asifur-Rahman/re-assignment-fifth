409cc684f369b2f10ad4ca1540574f254c72bdff
Asifur Rahman <asifurrahman.ac@gmail.com>
2024-12-06 23:27:14 +0600

    modal set done

--- index.js
+++ index.js
@@ -1,41 +1,61 @@
-let balance = 7000 
-const get_balance = document.getElementById('total-amount')
-get_balance.innerText = balance
+let balance = 7000;
+const get_balance = document.getElementById("total-amount");
+get_balance.innerText = balance;
 
-function calculate_fund(balance_value,donation){
-    return balance_value- donation
+function calculate_balance(balance_value, donation) {
+  return balance_value - donation;
 }
 
-// route click section 
-const donation_section = document.getElementById('donation-section')
-const history_section = document.getElementById('history-section')
+// route click section
+const donation_section = document.getElementById("donation-section");
+const history_section = document.getElementById("history-section");
 
-document.getElementById('donation').addEventListener('click',function(event){
-    event.target.style.backgroundColor = '#B4F461'
-    event.target.style.border = '0px'
-    document.getElementById('history').style.backgroundColor=''
-    document.getElementById('history').style.border='1px solid rgba(17, 17, 17, 0.3)'
-    history_section.classList.add('hidden')
-    donation_section.classList.remove('hidden')
+document.getElementById("donation").addEventListener("click", function (event) {
+  event.target.style.backgroundColor = "#B4F461";
+  event.target.style.border = "0px";
+  document.getElementById("history").style.backgroundColor = "";
+  document.getElementById("history").style.border =
+    "1px solid rgba(17, 17, 17, 0.3)";
+  history_section.classList.add("hidden");
+  donation_section.classList.remove("hidden");
+});
 
-})
+document.getElementById("history").addEventListener("click", function (event) {
+  event.target.style.backgroundColor = "#B4F461";
+  event.target.style.border = "0px";
+  document.getElementById("donation").style.backgroundColor = "";
+  document.getElementById("donation").style.border =
+    "1px solid rgba(17, 17, 17, 0.3)";
+  donation_section.classList.add("hidden");
+  history_section.classList.remove("hidden");
+});
 
-document.getElementById('history').addEventListener('click',function(event){
-    event.target.style.backgroundColor = '#B4F461'
-    event.target.style.border = '0px'
-    document.getElementById('donation').style.backgroundColor=''
-    document.getElementById('donation').style.border='1px solid rgba(17, 17, 17, 0.3)'
-    donation_section.classList.add('hidden')
-    history_section.classList.remove('hidden')
-})
+// fund add and money decrease section
 
-// fund add and money decrease section 
-
-// get click button 
-const get_button = donation_section.querySelectorAll('button')
-for(const button of get_button){
-    button.onclick = function (event){
-        const get_parent = event.target.parentNode
-        console.log(get_parent)
+// get click button
+const get_button = donation_section.querySelectorAll("button");
+for (const button of get_button) {
+  button.onclick = function (event) {
+    const get_parent = event.target.parentNode;
+    const get_input_value = get_parent.querySelector("input").value;
+    if (isNaN(get_input_value) || get_input_value === "") {
+      get_parent.querySelector("input").value = "";
+      alert("Enter a valid number");
+    } else {
+      let donate = parseInt(get_input_value);
+      if (donate > balance) {
+        get_parent.querySelector("input").value = "";
+        alert("Insufficient Amount");
+      } else {
+        balance = calculate_balance(balance, donate);
+        get_balance.innerText = balance
+        get_parent.querySelector("input").value = "";
+        const fund_value = parseInt(get_parent.querySelector('span').innerText)
+        const total_fund = fund_value + donate
+        get_parent.querySelector('span').innerText = total_fund
+        const get_modal = document.getElementById('my_modal_5')
+        get_modal.showModal()
+      }
     }
+  };
 }
